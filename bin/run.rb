b3dc2dff92083e12865f6cfbbcd8cf7b1c5bf2c8
github username <github email address>
2019-06-14 10:11:55 -0400

    Friday afternoon

--- bin/run.rb
+++ bin/run.rb
@@ -3,3 +3,14 @@ require_relative '../db/seed'
 require_relative '../bin/user_interface'
 
 efficient_chef = Program.new()
+user = efficient_chef.get_user
+efficient_chef.print_recipes
+recipe_id = efficient_chef.get_recipe_selection
+while recipe_id != "quit"
+    recipe_id = efficient_chef.display_recipe(recipe_id)
+    efficient_chef.execute_and_show_marginal(user, recipe_id)
+    recipe_id = efficient_chef.get_recipe_selection
+end
+efficient_chef.view_shopping_list(user)
+
+
--- bin/user_interface.rb
+++ bin/user_interface.rb
@@ -1,67 +1,79 @@
 class Program
 
-    def initialize
-prompt = TTY::Prompt.new
 
-input = prompt.ask("Thanks for opening The Efficient Chef. This is a command-line-interface tool meant to help optimize your grocery list. First, what is your name?")
-user = User.new(name: input)
+    def get_user
+    prompt = TTY::Prompt.new
+    input = prompt.ask("Thanks for opening The Efficient Chef. This is a command-line-interface tool meant to help optimize your grocery list. First, what is your name?")
+    user = User.create(name: input)
+    end
 
-#print out all the recipes
-Recipe.all.each do |recipe|
+    def print_recipes
+    #print out all the recipes
+    puts "
+    "
+    Recipe.all.each do |recipe|
     puts "[#{recipe.id}] #{recipe.name}"
-end
+    end
+    end
 
-recipe_id = prompt.ask("All available recipes are listed above. Please enter the number of your recipe selection, or 'quit' to exit")
+    def get_recipe_selection
+    prompt = TTY::Prompt.new
+    recipe_id = prompt.ask("Enter the ID of the recipe you would like to add, or 'quit' to exit")
+    end
 
-while recipe_id != "quit" #starts a while loop. users enter 'quit' to quit and print their shopping list
+    def display_recipe(recipe_id)
+    prompt = TTY::Prompt.new
 
-anchor_selection = Recipe.all.find_by(id: recipe_id)
-ingredients = anchor_selection.recipe_ingredients
-new_ingredients = anchor_selection.groceries_needed(user)
+    anchor_selection = Recipe.all.find_by(id: recipe_id)
+    ingredients = anchor_selection.recipe_ingredients
 
-puts "
+    puts "
 *****************************
 #{anchor_selection.name}
 Servings: #{anchor_selection.servings}
 *****************************
 
-"
-ingredients.map do |ri|
-puts "#{ri.ingredient.name}: #{ri.quantity.to_f} #{ri.ingredient.measure}"
+    "
+    ingredients.map do |ri|
+    puts "#{ri.ingredient.name}: #{ri.quantity.to_f} #{ri.ingredient.measure}"
+    end
+
+    puts "
+Directions: #{anchor_selection.process}
+    "
+    response = prompt.yes?("You have chosen ##{recipe_id}: #{Recipe.all.find_by(id: recipe_id).name}. The recipe is displayed. Do you want to select this recipe?")
+    if response == true
+        recipe_id
+    else
+        self.print_recipes
+        response = self.get_recipe_selection
+        response
+    end
 end
 
-puts "
-
-Directions: #{anchor_selection.process}
-
-"
-
-
-response = prompt.yes?("You have chosen ##{recipe_id}: #{Recipe.all.find_by(id: recipe_id).name}. The recipe is displayed. Do you want to select this recipe?")
-    user.do_recipe(Recipe.all.find_by(id: recipe_id))
+def execute_and_show_marginal(user, recipe_id)
+    user.do_recipe(Recipe.all.find_by(id: recipe_id.to_i))
     marginal_recipes = user.marginal_cost_recipes
     marginal_recipes = marginal_recipes.sort_by do |recipe, cost|
         cost
     end
     puts "Great. You now have #{Meal.all.select do |meal| meal.user == user end.size} meals taken care of"
     puts "But you still have more meals to plan! Here are additional recipes you could make with a low cost:"
-puts "
+    puts "
 
 **************************
 Marginal cost recipes
 **************************
 Recipe ID   Name        Marginal cost"
 
-marginal_recipes.map do |recipe, cost|
-puts "#{recipe.id}  #{recipe.name}:  $#{cost.round(2)}"
+    marginal_recipes.map do |recipe, cost|
+    puts "#{recipe.id}  #{recipe.name}:  $#{cost.round(2)}"
 
+    end
 end
 
 
-recipe_id = prompt.ask("Which recipe would you like to add? Enter 'quit' to exit")
-
-end #ends the while loop. This allows users to exit by entering 'quit'
-
+def view_shopping_list(user)
 shopping_list = user.shopping_list
 puts "
 
@@ -77,13 +89,11 @@ shopping_list.map do |ingredient, quantity|
 puts "#{ingredient.name} | #{ingredient.quantity_at_grocery} #{ingredient.measure} | #{quantity}"
 end
 
-
-
-
-
-
 end
 
-binding.pry
+
+
+
+
 
 end
